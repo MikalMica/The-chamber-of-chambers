48ea8a4ac26db782c410bda6ebba135d68664e31
Mika <mika@ucm.es>
2025-02-19 03:22:41 +0100

    Se pegan varios

--- The-Chamber-Of-Chambers/Assets/Scripts/ChamberHandler.cs
+++ The-Chamber-Of-Chambers/Assets/Scripts/ChamberHandler.cs
@@ -1,21 +1,22 @@
 using System;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class ChamberHandler : MonoBehaviour
 {
-    ChamberBrain _selectedChamber;
-
+    Queue<ChamberBrain> _selectedChambers = new Queue<ChamberBrain>();
     private void Update() {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
         if(Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity)) 
         {
             if(hit.collider.TryGetComponent(out ChamberBrain chamberBrain)) {
-                if(Input.GetMouseButtonDown(0)) {
-                    _selectedChamber = chamberBrain;
+                if(Input.GetMouseButtonDown(0) && (Input.GetKey(KeyCode.LeftShift) || _selectedChambers.Count < 1)) {
+                    _selectedChambers.Enqueue(chamberBrain);
                 }
             }else if(hit.collider.TryGetComponent(out HealthHandler healthHandler)) {
                 if(Input.GetMouseButtonDown(0)) {
+<<<<<<< HEAD
                     if(_selectedChamber != null) {
                         _selectedChamber.SetTarget(healthHandler);
                     }
@@ -24,6 +25,17 @@ public class ChamberHandler : MonoBehaviour
                 if(Input.GetMouseButtonDown(0)) {
                     if(_selectedChamber != null) {
                         _selectedChamber.GotoPosition(hit.point);
+=======
+                    
+                    int a_nChambers = _selectedChambers.Count; 
+                    ChamberBrain a_chamber;
+
+                    for(int i = 0; i < a_nChambers; ++i){
+                        a_chamber = _selectedChambers.Dequeue();
+                        if(a_chamber != null){
+                            a_chamber.SetTarget(resource);
+                        }
+>>>>>>> ff64e01 (Se pegan varios)
                     }
                 }
             }
